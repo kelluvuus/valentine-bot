9761923a9a958d16949ef2ed902503c958cccf2d
kelluvuus <lindverka@gmail.com>
2025-02-11 23:16:35 +0100

    Fix import error for executor

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
